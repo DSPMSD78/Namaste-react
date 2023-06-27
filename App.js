0d734e4122339977305f866b2b8fc6fe079d66e1
DSPMSD78 <prasad.duggirala.369@gmail.com>
2023-06-27 21:59:42 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,20 @@
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Hello world from React"
+);
+
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I'm a h1 Tag"),
+    React.createElement("h2", {}, "I'm a h2 Tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "I'm a h1 Tag"),
+    React.createElement("h2", {}, "I'm a h2 Tag"),
+  ]),
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
